6b1425898df2dc54630f81ec1ab0e764a7665607
alimlk4288 <alimlk4288@gmail.com>
2025-02-19 09:43:36 +0500

    Deployment

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
 def main():
     print("Hello, World!")
 
 if __name__ == "__main__":
     main()
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, DevOps!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+>>>>>>> 23038f3 (Deployment)
